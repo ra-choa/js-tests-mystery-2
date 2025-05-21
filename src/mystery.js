6f4e4efd442edd3aaab5f0237e21e18356df5b4d
Sno <ochoa.sara2036@gmail.com>
2025-05-21 11:18:57 -0700

    so many exclamation points.

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,11 @@
-export const mystery2 = (num) => {};
+export const mystery2 = (num) => {
+  if (num % 4) {
+    return false;
+  } if (!(num % 400)) {
+    return true;
+  } if (!(num % 100)) {
+    return false;
+  } if (!(num % 4)) {
+    return true;
+  }
+};
